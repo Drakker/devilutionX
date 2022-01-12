9201867e866ba95d9cfe16001bbc7cce5bc612a0
Drakker <dpelletier@gmail.com>
2022-01-11 22:48:01 -0500

    Added more Quickspell hotkeys for a total of 16. Saving will write the number of configured quickspell slots at the beginning of the hotkeys file. The loader will check the size, if its bigger than older saves should be it will load the header and read the hotkeys, otherwise it will load 4 quickspells for compatibility.

--- Source/player.h
+++ Source/player.h
@@ -33,6 +33,7 @@ namespace devilution {
 #define MAX_SPELL_LEVEL 15
 #define PLR_NAME_LEN 32
 
+static constexpr size_t NumHotkeys = 16;
 constexpr int BaseHitChance = 50;
 
 /** Walking directions */
@@ -231,8 +232,8 @@ struct Player {
 	uint64_t _pAblSpells;  // Bitmask of abilities
 	uint64_t _pScrlSpells; // Bitmask of spells available via scrolls
 	uint8_t _pSpellFlags;
-	spell_id _pSplHotKey[4];
-	spell_type _pSplTHotKey[4];
+	spell_id _pSplHotKey[NumHotkeys];
+	spell_type _pSplTHotKey[NumHotkeys];
 	bool _pBlockFlag;
 	bool _pInvincible;
 	int8_t _pLightRad;
